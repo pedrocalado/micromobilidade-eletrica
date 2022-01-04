43d2f90af796c9fb61dd29a78e3f9e74cd67c4f2
pedrocalado <pcalado99@gmail.com>
2022-01-04 23:51:49 +0000

    Restructurin index.js and configuration variables

--- service_users/app.js
+++ service_users/app.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +11,4 @@ app.use(cors());
 
 app.use('/', routes)
 
-app.listen(PORT, () => {
-    console.log(`servidor a executar em http://localhost:${PORT}`);
-});
+module.exports = app;
