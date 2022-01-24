91cb01685bfe62b671ea11b7dc20fabca27c8059
pedrocalado <pcalado99@gmail.com>
2022-01-24 22:20:10 +0000

    Rental auth and admin middlewares and missing packages

---
+++ service_rental/middlewares/admin.js
@@ -0,0 +1,29 @@
+const config = require('../config/config');
+const axios = require('axios');
+
+const admin = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(403);
+
+    // Check it the user is admin
+    try {
+        const usersServiceUrl = config.usersService.admin;
+        const isAdmin = await axios.post(usersServiceUrl, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if (isAdmin.data.is_admin) {
+            next();
+        } else {
+            return res.sendStatus(403);
+        }
+    } catch (err) {
+        return res.sendStatus(403);
+    }
+}
+
+module.exports = admin;
---
+++ service_rental/middlewares/auth.js
@@ -0,0 +1,29 @@
+const config = require('../config/config');
+const axios = require('axios');
+
+const auth = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(403);
+
+    // Check it the token is valid in the users service
+    try {
+        const usersServiceUrl = config.usersService.auth;
+        const isAuth = await axios.post(usersServiceUrl, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if (isAuth.data.is_authenticated) {
+            next();
+        } else {
+            return res.sendStatus(403);
+        }
+    } catch (err) {
+        return res.sendStatus(403);
+    }
+}
+
+module.exports = auth;
