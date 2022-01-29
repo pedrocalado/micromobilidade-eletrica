7aff4eac829219713181be1a85d00f909e6ef89d
pedrocalado <pcalado99@gmail.com>
2022-01-29 16:38:37 +0000

    Remove NODE_ENV from configs

--- service_rental/config/config.js
+++ service_rental/config/config.js
@@ -6,7 +6,6 @@ const Joi = require('joi');
 
 const envVarsSchema = Joi.object()
     .keys({
-        NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
         PORT: Joi.number().default(3000),
         MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     })
@@ -19,10 +18,9 @@ if (error) {
 }
 
 module.exports = {
-    env: envVars.NODE_ENV,
     port: envVars.PORT,
     mongoose: {
-        url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+        url: envVars.MONGODB_URL,
         options: {
             // useCreateIndex: true,
             useNewUrlParser: true,
